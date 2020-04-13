d1b18bd612baa88f09eb395431234fed41b1d165
sanderblue <william.a.blue@gmail.com>
2020-04-13 22:36:04 +0000

    newrelic-cli 0.6.70

--- Formula/newrelic-cli.rb
+++ Formula/newrelic-cli.rb
@@ -1,8 +1,8 @@
 class NewrelicCli < Formula
   desc "The New Relic Command-line Interface"
   homepage "https://github.com/newrelic/newrelic-cli"
-  url "https://github.com/newrelic/newrelic-cli/archive/v0.6.1.tar.gz"
-  sha256 "d910dc8ff0898ee7ebf64f1f326d16c0e47cf02b4d79168617daa06f86aa2af2"
+  url "https://github.com/newrelic/newrelic-cli/archive/v0.6.70.tar.gz"
+  sha256 "b0864d24fc68908d83b4ecc269ff1743f662e914734e5e4a4d2e471fc3790ef8"
   head "https://github.com/newrelic/newrelic-cli.git"
 
   bottle do
